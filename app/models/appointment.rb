8cb39d9f9651422392ea6ab52d0f7e32fefde084
Christine <christinemelia@gmail.com>
2012-03-13 15:22:15 +0000

    init

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -7,5 +7,5 @@ class Appointment < ActiveRecord::Base
   	validates :date,  :presence => true
   	validates :hour,  :presence => true,
   	                  :uniqueness => {:scope => :date}
-  	end
+  
 end
