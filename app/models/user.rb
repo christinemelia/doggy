e23d8ec3c38a7d82e999b7845fc97fad1bcb6512
Christine <christinemelia@gmail.com>
2012-03-13 15:07:37 +0000

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 
 class User < ActiveRecord::Base
   
-has_many :authentications
+#has_many :authentications
 
 validates :name, presence: true, uniqueness: true
 has_secure_password
@@ -10,12 +10,12 @@ after_destroy :ensure_an_admin_remains
 
 
 
-def add_provider(auth_hash)
+#def add_provider(auth_hash)
   # Check if the provider already exists, so we don't add it twice
-  unless authentications.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+ # unless authentications.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
     Authentication.create :user => self, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
-  end
-end
+ # end
+#end
 
 
   private
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,19 +1,19 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
+# Rails.application.config.middleware.use OmniAuth::Builder do
   
-  if Rails.env == 'development' || Rails.env == 'test'
-  provider :twitter, 'SMuCQMkwpjjeISXo2l1kA', 'TX3sDCUyvidR4XEbIbX8myC6hc9g62Aujy8dt6tXE'
-  provider :google_oauth2, ['GOOGLE_KEY'], ['GOOGLE_SECRET']
-  provider :facebook, '371054002912427', 'fa34419be9da833b9dadc3134f26591c'
+  # if Rails.env == 'development' || Rails.env == 'test'
+  # provider :twitter, 'SMuCQMkwpjjeISXo2l1kA', 'TX3sDCUyvidR4XEbIbX8myC6hc9g62Aujy8dt6tXE'
+  # provider :google_oauth2, ['GOOGLE_KEY'], ['GOOGLE_SECRET']
+  # provider :facebook, '371054002912427', 'fa34419be9da833b9dadc3134f26591c'
   
 
   
-  else
+  # else
     # Production
-    Rails.application.config.middleware.use OmniAuth::Builder do
-      provider :facebook, '371054002912427', 'fa34419be9da833b9dadc3134f26591c'
-    end
-  end
-end
+    # Rails.application.config.middleware.use OmniAuth::Builder do
+    #   provider :facebook, '371054002912427', 'fa34419be9da833b9dadc3134f26591c'
+    # end
+  # end
+# end
 
 
 
