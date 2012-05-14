72fec83c6d06fa4af38a190eaded1152cb177c9d
Christine <christinemelia@gmail.com>
2012-05-14 04:51:53 +0100

    Configured for deployment to Heroku

--- db/migrate/20120424153921_add_details_to_orders.rb
+++
@@ -1,8 +0,0 @@
-class AddDetailsToOrders < ActiveRecord::Migration
-  def change
-    add_column :orders, :cart_id, :integer
-    add_column :orders, :ip_address, :string
-    add_column :orders, :card_type, :string
-    add_column :orders, :card_expires_on, :date
-  end
-end
